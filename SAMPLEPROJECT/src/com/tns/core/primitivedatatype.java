fd233fa133f3d828eb41558bf75b0b9dd2bed7d5
DELL <DELL@DESKTOP-NNAGLSN>
2022-08-02 07:22:46 +0530

    Java primitivedatatype

---
+++ SAMPLEPROJECT/src/com/tns/core/primitivedatatype.java
@@ -0,0 +1,73 @@
+package com.tns.core;
+public class primitivedatatype
+{
+  public static void main(String[] args) 
+	/*{
+		boolean flag = true;//boolean type = They are usually used for true/false conditions.
+	    System.out.println(flag); // prints true
+	}*/
+	
+	
+	/*{
+		byte range;//byte type = data type can have values from -128 to 127 (8-bit signed two's complement integer).
+	    range = 124;
+	    System.out.println(range);    // prints 124 
+	}*/
+	
+	
+	/*{
+		short temperature; //short type = data type in Java can have values from -32768 to 32767 (16-bit signed two's complement integer).
+	    temperature = -200;
+	    System.out.println(temperature);  // prints -200
+	}*/
+	
+	
+	/*{
+		int range = -4250000;// int type = data type can have values from -231 to 231-1 (32-bit signed two's complement integer).
+	    System.out.println(range);  // print -4250000
+	}*/
+	
+	
+	/*{
+		long range = -42332200000L; //long type =  data type can have values from -263 to 263-1 (64-bit signed two's complement integer).
+	    System.out.println(range);    // prints -42332200000
+	}*/
+	
+	
+	/*{
+		double number = -42.3; //double type = data type is a double-precision 64-bit floating-point.
+	    System.out.println(number);  // prints -42.3
+	}*/
+    
+	
+	/*{
+		float number = -42.3f; //float type =data type is a single-precision 32-bit floating-point.
+	    System.out.println(number);  // prints -42.3
+	}*/
+	
+    
+	/*{
+		    char letter = '\u0051'; // char type =a 16-bit Unicode character.minimum value ='\u0000' (0) and the maximum value of the is '\uffff'.
+		    System.out.println(letter);  // prints Q (Unicode value of Q is \u0051.)
+    }*/
+	//OTHER EXAMPLE(CHAR)
+	/*{
+	    char letter1 = '9';
+	    System.out.println(letter1);  // prints 9
+	    	
+	    char letter2 = 65;
+	    System.out.println(letter2);  // prints A(ASCII value of A is 65.)
+	  }*/
+	
+	{
+		// create strings
+	    String first = "Java";
+	    String second = "Python";
+	    String third = "JavaScript";
+
+	    // print strings
+	    System.out.println(first);   // print Java
+	    System.out.println(second);  // print Python
+	    System.out.println(third);   // print JavaScript
+	  }
+	}
